0df2ee93f7d66e2f072cd68f7ef7539f72b2b3f6
Mystist <gjl87910lq@gmail.com>
2024-10-08 01:07:26 -0700

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="font-sans items-center justify-items-center min-h-screen p-8">
+      dapp
+    </div>
+  );
+}
