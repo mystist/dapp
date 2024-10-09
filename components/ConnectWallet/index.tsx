dc2383cbd212d72c8e1d5d6fd3bb4c4fea1a8131
Mystist <gjl87910lq@gmail.com>
2024-10-09 00:23:44 -0700

    feat: switch chain

--- components/ConnectWallet/index.tsx
+++ components/ConnectWallet/index.tsx
@@ -3,14 +3,15 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { useCallback, useMemo } from 'react'
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { useAccount, useConnect, useDisconnect, useSwitchChain } from 'wagmi'
 
 import { copyToClipboard, shortenAddress } from '@/utils'
 
 export default function Home() {
   const { connectors, connect } = useConnect()
-  const { address } = useAccount()
+  const { address, chain } = useAccount()
   const { disconnect } = useDisconnect()
+  const { chains, switchChain } = useSwitchChain()
 
   const connector = useMemo(() => {
     return connectors.find((item) => item.id === 'injected')
@@ -43,6 +44,32 @@ export default function Home() {
         <Menu as="div" className="relative inline-block text-left">
           <div>
             <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 font-medium text-gray-900 hover:bg-gray-50">
+              {chain ? chain.name : 'unsupported chain'}
+              <ChevronDownIcon aria-hidden="true" className="-mr-1 h-5 w-5 text-gray-400" />
+            </MenuButton>
+          </div>
+
+          <MenuItems
+            transition
+            className="absolute right-0 z-10 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+          >
+            <div className="py-1">
+              {chains.map((item) => (
+                <MenuItem key={item.id}>
+                  <button onClick={() => switchChain({ chainId: item.id })} className="block w-full px-4 py-2 text-left text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900">
+                    {item.name}
+                  </button>
+                </MenuItem>
+              ))}
+            </div>
+          </MenuItems>
+        </Menu>
+      )}
+
+      {address && (
+        <Menu as="div" className="relative inline-block text-left">
+          <div>
+            <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 font-medium text-gray-900 hover:bg-gray-50">
               {shortenAddress(address)}
               <ChevronDownIcon aria-hidden="true" className="-mr-1 h-5 w-5 text-gray-400" />
             </MenuButton>
@@ -50,7 +77,7 @@ export default function Home() {
 
           <MenuItems
             transition
-            className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+            className="absolute right-0 z-10 mt-2 origin-top-right text-nowrap rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
           >
             <div className="py-1">
               <MenuItem>
